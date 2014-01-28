042d3c099560a0ef4b93404d0f5d306c67822bbd
Nathan Rogers <nrogers@unithq.com>
2014-01-27 20:18:18 -0500

    SVG bar graph now sits on a pretty baseline

--- assets/js/d3tutorial.js
+++ assets/js/d3tutorial.js
@@ -4,13 +4,41 @@
 // experience
 console.log('STARTING D3 TUTORIAL')
 
-var dataset = [2.5, 2.9, 3.5, 4.5, 3.9, 4.2]
-d3.select('div.barChart')
-    .selectAll('div')
-    .data(dataset)
-    .enter()
-    .append('div')
-    .classed('value', true)
-    .style("height", function(d) {
-      return (d/5)*100 + "px";
-    }); 
+var dataset = []
+while (dataset.length < 20) {
+  // Generate a random number between 0.5 and 5.0
+  seed = Math.random()*5;
+  seed = Math.min(seed, 5);
+  seed = Math.max(seed, 0.5);
+
+  dataset.push(seed);
+}
+
+var svg_dimensions = {height: 300, width: 500};
+var bar_dimensions = {width: 20};
+var baseline = 50;
+
+var svg = d3.select('div#charts')
+            .append('svg')
+            .attr('id', 'ratings_distribution')
+            .attr('height', svg_dimensions['height'])
+            .attr('width', svg_dimensions['width'])
+  
+var graph_values = svg.selectAll('rect')
+                      .data(dataset)
+                      .enter()
+                      .append('rect')
+graph_values.attr('width', bar_dimensions['width'])
+  .attr('height', function(d) {
+      return Math.round(d*60);
+    }) 
+  .attr('x', function(d, i) {
+      return (i*bar_dimensions['width']*1.5);
+    })
+  .attr('y', function(d, i) {
+      console.log("Element value => " + Math.round(d*80));
+      console.log("Y offset for element " + i + " => " + 
+        (svg_dimensions['height']-Math.round(d*80)))
+      return svg_dimensions['height']-(d*60);
+    })
+  .classed('graph', true)
